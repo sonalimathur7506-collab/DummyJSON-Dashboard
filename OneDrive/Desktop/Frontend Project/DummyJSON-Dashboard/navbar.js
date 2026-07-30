c5614e681099d1ed95ea4ae8abb3edad1863c467
sonalimathur7506-collab <sonalimathur7506@gmail.com>
2026-07-31 00:09:57 +0530

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/Frontend Project/DummyJSON-Dashboard/navbar.js
+++
@@ -1,8 +0,0 @@
-const menuBtn = document.getElementById("menuBtn");
-const navLinks = document.getElementById("navLinks");
-
-menuBtn.addEventListener("click", () => {
-
-    navLinks.classList.toggle("active");
-
-});
